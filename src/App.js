4fc6383a0867bf77ed3a6bf1e857ce2374a41cca
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-10-18 19:21:25 -0500

    begin contact form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery></Gallery>
         <About></About>
       </main>
